c9bd6477b5d35793d36d86c701780d0532f47850
Yuto Mabe <yuto96315mabe@gmail.com>
2022-07-13 11:36:38 +0900

    Initial Commit

---
+++ TinderCloneApp/TinderCloneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderCloneAppApp.swift
+//  TinderCloneApp
+//
+//  Created by 間部雄斗 on 2022/07/13.
+//
+
+import SwiftUI
+
+@main
+struct TinderCloneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
